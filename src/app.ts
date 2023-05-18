2cb15e521791456afcdb3b595f7cf635550a42b9
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-05-18 12:15:56 +0100

    Server up

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,45 @@ import { sequelize } from './config/database';
 import { log } from './utils/logger';
 import { messages } from './utils/consts';
 import userRoutes from './routes/user.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 
 const app : Express = express();
+
+const swaggerOptions = {
+  failOnErrors: true,
+  definition : {
+    openapi: '3.0.0',
+    info : {
+      version : "1.0.0",
+      title : "Documentation for virtual pay"
+    }
+  },
+  apis : [ __dirname+'/swagger/*.js' ],
+  components : {
+    securitySchemes : {
+      bearerAuth : {
+        type : 'http',
+        scheme : 'bearer',
+        bearerFormer : 'JWT'
+      }
+    }
+  }
+}
 
 sequelize.sync({ alter : true }).then(async ()=>{
     log('info', messages.DB_CONNECTED);
     app.listen(process.env.PORT, ()=>{ log('info', messages.SERVER_STARTED) });
 }).catch(err=>{ log('error', { message : messages.SERVER_FAILED_TO_START, err }); });
 
-app.use(cors({ origin : process.env.GATEWAY_BASE_URL }));
+app.use(cors());
 app.use(express.json({ limit : '50mb' }));
 
 app.use('/user', userRoutes);
-
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerJSDoc(swaggerOptions), { explorer : true })
+  );
+  
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import { Table, Model, Column, DataType } from 'sequelize-typescript'
 
 @Table
 export class User extends Model{
-    @Column({ type : DataType.UUID, allowNull : false, primaryKey : true, autoIncrement : true })
+    @Column({ type : DataType.INTEGER, allowNull : false, primaryKey : true, autoIncrement : true })
     public id!: number
 
     @Column({ type : DataType.STRING, allowNull : false })
